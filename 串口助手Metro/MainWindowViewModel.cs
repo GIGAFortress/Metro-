e36114381e825ae283f7428f7e35db1ad7224281
GIGAFortress <clj8196@126.com>
2015-08-28 17:44:33 +0800

    进一步的使用属性包装器进行修正使之更好看代码更简洁

--- 串口助手Metro/MainWindowViewModel.cs
+++ 串口助手Metro/MainWindowViewModel.cs
@@ -43,45 +43,18 @@ namespace 串口助手Metro
             get { return _ShowTimeCheckBox; }
             set { _ShowTimeCheckBox = value; OnPropertyChanged("ShowTimeCheckBox"); }
         }
-        #region 串口设置相关的一系列属性
-        private string[] _PortNames;
+        #region 串口设置相关的一系列属性        
+        public string[] PortNames { get { return SerialPort.GetPortNames(); } }
+        
+        private string[] _BaudRates = { "1200", "2400", "4800", "9600", "14400", "19200", "38400", "56000", "57600", "115200" };
+        public string[] BaudRates{ get { return _BaudRates; } }
 
-        public string[] PortNames
-        {
-            get { return _PortNames; }
-            set { _PortNames = value; OnPropertyChanged("PortNames"); }
-        }
+        private string[] _DataBits = { "6", "7", "8" };
+        public string[] DataBits { get { return _DataBits; } }
+              
+        public string[] Parities { get { return Enum.GetNames(typeof(Parity)); } }
 
-        private string[] _BaudRates;
-
-        public string[] BaudRates
-        {
-            get { return _BaudRates; }
-            set { _BaudRates = value; OnPropertyChanged("BaudRates");}
-        }
-        private string[] _DataBits;
-
-        public string[] DataBits
-        {
-            get { return _DataBits; }
-            set { _DataBits = value;OnPropertyChanged("DataBits"); }
-        }
-
-        private string[] _Parities;
-
-        public string[] Parities
-        {
-            get { return _Parities; }
-            set { _Parities = value; OnPropertyChanged("Parities"); }
-        }
-
-        private string[] _StopBits;
-
-        public string[] StopBits
-        {
-            get { return _StopBits; }
-            set { _StopBits = value;OnPropertyChanged("StopBits"); }
-        }
+        public string[] StopBits { get { return Enum.GetNames(typeof(StopBits)); } }
         #endregion
         private bool _PortToggleSwitchIsChecked;
 
@@ -117,8 +90,8 @@ namespace 串口助手Metro
             set { Port.StopBits = value; }
         }
         #endregion
-        string[] BaudRatesList = { "1200", "2400", "4800", "9600", "14400", "19200", "38400", "56000", "57600", "115200" };
-        string[] DataBitsList = { "6", "7", "8" };
+        
+        
         
 
         public DelegateCommand ClearButtonCommand { get; set; }
@@ -135,16 +108,9 @@ namespace 串口助手Metro
         public SerialPort Port = new SerialPort();
 
         public MainWindowViewModel()
-        {
-            PortNames = SerialPort.GetPortNames();            
-            BaudRates = BaudRatesList;
-            DataBits = DataBitsList;
-            Parities = Enum.GetNames(typeof(Parity));
-            //ReceiveDataTextBox = string.Join(",",Enum.GetNames(typeof(Parity)));
-            StopBits = Enum.GetNames(typeof(StopBits));
+        {            
             ClearButtonCommand = new DelegateCommand(_ClearButtonCommand);
             SendButtonCommand = new DelegateCommand(new Action(_SendButtonCommand));
-            
         }
         
     }
